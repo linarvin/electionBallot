d95b0fff0ce7c06f16a04da8ff7b514018773be2
Ryn888 <49345298+Ryn888@users.noreply.github.com>
2020-11-16 10:37:29 -0500

    Update electionBallot.py
    This version takes more inputs but there is a bug where if you change your vote, it will only accept the first vote

--- electionBallot.py
+++ electionBallot.py
@@ -1,14 +1,20 @@
 
+
 import cmd,sys
 
 
 #Main Function
+canidates = {'1':"Rick Sanchez",'2':"Charlie Brown"}
+
 def main():
-    print("Welcome to your election ballot")
-    name = getName()
-    print(name)
-    idNum = getID()
-    print(idNum)
+    #canidates = {'1':"Rick Sanchez",'2':"Charlie Brown"}
+##    print("Welcome to your election ballot")
+##    name = getName()
+##    print(name)
+##    idNum = getID()
+##    print(idNum)
+    vote = getVote()
+    print(vote)
 
 #gets name
 def getName():
@@ -28,7 +34,7 @@ def checkName(text):
 
 #gets id
 def getID():
-    idNum = input("Please enter First and Last Name (ex:Jane Doe)\n")
+    idNum = input("Please enter you ID number\n")
     checkID(idNum)
     return idNum
 
@@ -43,6 +49,42 @@ def checkID(text):
         checkID(text)
 
 #will add function to check if name and id are already in system
+##def checkRegistration:
+##    return
+
+def getVote():
+    while True:
+        print("The canidates are as follow: ")
+        print("1. Rick Sanchez")
+        print("2. Charlie Brown")
+        vote = input("Enter the number coresponding to the canidate you choose.")
+        vote2 = str(vote)
+        #print(vote2)
+        #print(type(vote2))
+        if((vote2 == '1') or (vote2 == '2')):
+            myVote = checkVote(vote2)
+            print(myVote)
+            return myVote
+        print("Invalid Number")
+        
+
+def checkVote(num):
+    name = canidates[num]
+    check = input("Is " + name + " correct(y/n)? " )
+    if(check == 'n'):
+        getVote()
+    elif(check == 'y'):
+        confirm = num
+        print(confirm)
+        return confirm
+    else:
+        checkVote(text)
+
+#store vote into file?
+#or write into new file
+def storeVote(name,ID,vote):
+    return
+        
 
 if __name__=="__main__": 
     main() 
